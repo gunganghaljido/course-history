ce5b6970d34f0811fefc228aa9f95f80d4786fb2
leemhoon00 <leemhoon000@gmail.com>
2024-11-08 22:55:21 +0900

    refactor: courseHistory 모듈로 분리

--- src/course/save-course-history.ts
+++ src/course/save-course-history.ts
@@ -1,53 +1,54 @@
-import * as fs from 'fs';
-import * as path from 'path';
-import { prisma } from '../prisma/client';
+import { PrismaClient } from '@prisma/client';
 
-export async function saveCourseHistory() {
-  const data = JSON.parse(
-    fs.readFileSync(path.join(__dirname, '../../history.json'), 'utf-8')
-  ) as CourseJson[];
-
-  const courses = [];
-  for (const course of data) {
-    if (
-      course.COURSE_BEGIN_DE === null ||
-      course.COURSE_END_DE === null ||
-      course.COURSE_REQST_NMPR_CO === '0'
-    ) {
-      continue;
-    }
-    const startDateStr = String(course.COURSE_BEGIN_DE);
-    const endDateStr = String(course.COURSE_END_DE);
-
-    const startDate = `${startDateStr.slice(0, 4)}-${startDateStr.slice(
-      4,
-      6
-    )}-${startDateStr.slice(6, 8)}`;
-    const endDate = `${endDateStr.slice(0, 4)}-${endDateStr.slice(
-      4,
-      6
-    )}-${endDateStr.slice(6, 8)}`;
-
-    courses.push({
-      businessId: course.BSNS_NO,
-      facilityName: course.FCLTY_NM,
-      itemCode: course.ITEM_CD,
-      itemName: course.ITEM_NM,
-      address: course.FCLTY_ADDR,
-      detailAddress: course.FCLTY_DETAIL_ADDR,
-      courseName: course.COURSE_NM,
-      courseId: course.COURSE_NO,
-      startDate: new Date(startDate),
-      endDate: new Date(endDate),
-      participantCount: Number(course.COURSE_REQST_NMPR_CO),
-      price: Number(course.COURSE_PRC),
-    });
+export class CourseHistoryService {
+  private prisma: PrismaClient;
+  constructor(prisma: PrismaClient) {
+    this.prisma = prisma;
   }
 
-  await prisma.courseHistory.createMany({
-    data: courses,
-    skipDuplicates: true,
-  });
+  async save(data: CourseJson[]) {
+    const courses = [];
+    for (const course of data) {
+      if (
+        course.COURSE_BEGIN_DE === null ||
+        course.COURSE_END_DE === null ||
+        course.COURSE_REQST_NMPR_CO === '0'
+      ) {
+        continue;
+      }
+      const startDateStr = String(course.COURSE_BEGIN_DE);
+      const endDateStr = String(course.COURSE_END_DE);
+
+      const startDate = `${startDateStr.slice(0, 4)}-${startDateStr.slice(
+        4,
+        6
+      )}-${startDateStr.slice(6, 8)}`;
+      const endDate = `${endDateStr.slice(0, 4)}-${endDateStr.slice(
+        4,
+        6
+      )}-${endDateStr.slice(6, 8)}`;
+
+      courses.push({
+        businessId: course.BSNS_NO,
+        facilityName: course.FCLTY_NM,
+        itemCode: course.ITEM_CD,
+        itemName: course.ITEM_NM,
+        address: course.FCLTY_ADDR,
+        detailAddress: course.FCLTY_DETAIL_ADDR,
+        courseName: course.COURSE_NM,
+        courseId: course.COURSE_NO,
+        startDate: new Date(startDate),
+        endDate: new Date(endDate),
+        participantCount: Number(course.COURSE_REQST_NMPR_CO),
+        price: Number(course.COURSE_PRC),
+      });
+    }
+
+    await this.prisma.courseHistory.createMany({
+      data: courses,
+      skipDuplicates: true,
+    });
+  }
 }
 
 type CourseJson = {
@@ -67,8 +68,8 @@ type CourseJson = {
   COURSE_NO: string; // 과정 코드
   COURSE_ESTBL_YEAR: string; // 과정 설립년도
   COURSE_ESTBL_MT: string; // 과정 설립월
-  COURSE_BEGIN_DE: string; // 과정 시작일
-  COURSE_END_DE: string; // 과정 종료일
+  COURSE_BEGIN_DE: string | null; // 과정 시작일
+  COURSE_END_DE: string | null; // 과정 종료일
   COURSE_REQST_NMPR_CO: string; // 과정 모집인원
   COURSE_PRC: string; // 과정 가격
 };
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
-import { saveCourseHistory } from './course/save-course-history';
+import { CourseHistoryService } from './course/save-course-history';
+import * as fs from 'fs';
+import * as path from 'path';
 import * as dotenv from 'dotenv';
+import { PrismaClient } from '@prisma/client';
 dotenv.config();
 
 async function main() {
-  await saveCourseHistory();
+  const prisma = new PrismaClient();
+  const courseHistoryService = new CourseHistoryService(prisma);
+
+  const data = JSON.parse(
+    fs.readFileSync(
+      path.join(__dirname, '../normal-course-history.json'),
+      'utf-8'
+    )
+  );
+
+  await courseHistoryService.save(data);
 }
 
 main();
